1978cae6e003538da2d9c746d8794f12ab6c4ab8
Nurdaulet <nurdauletdilbek@gmail.com>
2025-12-17 10:23:53 +0500

    Week1: initial project setup

---
+++ src/com/nurdaulet/vetclinic/Main.java
@@ -0,0 +1,9 @@
+package com.nurdaulet.vetclinic;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Welcome to VetClinic System");
+    System.out.println("This project will manage clients, pets, vets, and appointments.");
+    System.out.println("Author: Nurdaulet, Astana IT University, BDA");
+  }
+}
