0212582f2b1100d0524e9b62d197d140f737c645
Nurdaulet <nurdauletdilbek@gmail.com>
2026-01-07 21:19:31 +0500

    Week3

--- src/com/nurdaulet/vetclinic/Main.java
+++ src/com/nurdaulet/vetclinic/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     private static ArrayList<Pet> pets = new ArrayList<>();
     private static ArrayList<Owner> owners = new ArrayList<>();
+    private static ArrayList<Veterinarian> veterinarians = new ArrayList<>();
     private static Scanner scanner = new Scanner(System.in);
 
 
@@ -17,16 +18,22 @@ public class Main {
         pets.add(new Pet(1, "Buddy", "Dog", 3, false));
         pets.add(new Pet(2, "Kitty", "Cat", 1, true));
 
+        veterinarians.add(new Veterinarian(201, "Dr. Aibek", "Dog", 6, true));
+        veterinarians.add(new Veterinarian(202, "Dr. Dana", "Cat", 3, true));
+
+
         owners.add(new Owner(101, "Aruzhan", "+77011234567", 2, false));
         owners.add(new Owner(102, "Dias", "+77019998877", 1, false));
 
 
         while (running) {
-            System.out.println("\n=== VET CLINIC MENU ===");
+            System.out.println("=== VET CLINIC MENU ===");
             System.out.println("1. Add Pet");
             System.out.println("2. View All Pets");
             System.out.println("3. Add Owner");
             System.out.println("4. View All Owners");
+            System.out.println("5. Add Veterinarian");
+            System.out.println("6. View All Veterinarians");
             System.out.println("0. Exit");
             System.out.print("Choose option: ");
 
@@ -46,6 +53,13 @@ public class Main {
                 case 4:
                     viewAllOwners();
                     break;
+                case 5:
+                    addVeterinarian();
+                    break;
+
+                case 6:
+                    viewAllVeterinarians();
+                    break;
                 case 0:
                     running = false;
                     scanner.close();
@@ -55,6 +69,7 @@ public class Main {
                     System.out.println("Invalid choice!");
             }
         }
+        scanner.close();
     }
     private static void addPet() {
         System.out.println("--- ADD PET ---");
@@ -120,6 +135,39 @@ public class Main {
             System.out.println(owner);
         }
     }
+    private static void addVeterinarian() {
+        System.out.println("--- ADD VETERINARIAN ---");
+
+        System.out.print("Vet ID: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Specialization: ");
+        String specialization = scanner.nextLine();
+
+        System.out.print("Experience years: ");
+        int exp = scanner.nextInt();
+        scanner.nextLine();
+
+        veterinarians.add(new Veterinarian(id, name, specialization, exp, true));
+        System.out.println("Veterinarian added successfully!");
+    }
+    private static void viewAllVeterinarians() {
+        System.out.println("--- ALL VETERINARIANS ---");
+
+        if (veterinarians.isEmpty()) {
+            System.out.println("No veterinarians found.");
+            return;
+        }
+
+        for (Veterinarian v : veterinarians) {
+            System.out.println(v);
+        }
+    }
+
 
 
 
