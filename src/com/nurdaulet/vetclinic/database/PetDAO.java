de39ca05867c9cfabfe59344f767005f20df2545
Nurdaulet <nurdauletdilbek@gmail.com>
2026-01-28 18:32:53 +0500

    week 7 completed

--- src/com/nurdaulet/vetclinic/database/PetDAO.java
+++ src/com/nurdaulet/vetclinic/database/PetDAO.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PetDAO {
 
@@ -43,7 +45,7 @@ public class PetDAO {
 
     public void getAllPets() {
 
-        String sql = "SELECT * FROM pet";
+        String sql = "SELECT * FROM pet WHERE deleted = FALSE";
         Connection connection = DatabaseConnection.getConnection();
 
         try {
@@ -77,5 +79,235 @@ public class PetDAO {
             DatabaseConnection.closeConnection(connection);
         }
     }
+
+    public Pet getPetById(int petId) {
+
+        String sql = "SELECT * FROM pet WHERE pet_id = ? AND deleted = FALSE\n";
+        Connection connection = DatabaseConnection.getConnection();
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, petId);
+
+            ResultSet rs = statement.executeQuery();
+
+            if (rs.next()) {
+                return new Pet(
+                        rs.getInt("pet_id"),
+                        rs.getString("name"),
+                        rs.getString("species"),
+                        rs.getInt("age"),
+                        rs.getBoolean("vaccinated")
+                );
+            }
+
+            rs.close();
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.closeConnection(connection);
+        }
+
+        return null;
+    }
+
+    public boolean updatePet(Pet pet) {
+
+        String sql = "UPDATE pet SET name = ?, species = ?, age = ?, vaccinated = ? " +
+                "WHERE pet_id = ?";
+
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection == null) return false;
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, pet.getName());
+            statement.setString(2, pet.getSpecies());
+            statement.setInt(3, pet.getAge());
+            statement.setBoolean(4, pet.isVaccinated());
+            statement.setInt(5, pet.getPetId()); // WHERE
+
+            int rowsUpdated = statement.executeUpdate();
+            statement.close();
+
+            if (rowsUpdated > 0) {
+                System.out.println("Pet updated successfully!");
+                return true;
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Update failed!");
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.closeConnection(connection);
+        }
+
+        return false;
+    }
+
+    public boolean deletePet(int petId) {
+
+        String sql = "UPDATE pet SET deleted = TRUE WHERE pet_id = ?\n";
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection == null) return false;
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, petId);
+
+            int rowsDeleted = statement.executeUpdate();
+            statement.close();
+
+            if (rowsDeleted > 0) {
+                System.out.println("Pet deleted (ID: " + petId + ")");
+                return true;
+            } else {
+                System.out.println("No pet found with ID: " + petId);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Delete failed!");
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.closeConnection(connection);
+        }
+
+        return false;
+    }
+
+
+
+    public List<Pet> searchPetsByName(String name) {
+
+        List<Pet> pets = new ArrayList<>();
+
+        String sql = "SELECT * FROM pet WHERE name ILIKE ? AND deleted = FALSE";
+
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection == null) return pets;
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, "%" + name + "%");
+
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                Pet pet = new Pet(
+                        rs.getInt("pet_id"),
+                        rs.getString("name"),
+                        rs.getString("species"),
+                        rs.getInt("age"),
+                        rs.getBoolean("vaccinated")
+                );
+                pets.add(pet);
+            }
+
+            rs.close();
+            statement.close();
+
+        } catch (SQLException e) {
+            System.out.println("Search by name failed!");
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.closeConnection(connection);
+        }
+
+        return pets;
+    }
+
+
+
+
+    public List<Pet> searchPetsByAgeRange(int minAge, int maxAge) {
+
+        List<Pet> pets = new ArrayList<>();
+
+        String sql = "SELECT * FROM pet WHERE age BETWEEN ? AND ? AND deleted = FALSE";
+
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection == null) return pets;
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, minAge);
+            statement.setInt(2, maxAge);
+
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                Pet pet = new Pet(
+                        rs.getInt("pet_id"),
+                        rs.getString("name"),
+                        rs.getString("species"),
+                        rs.getInt("age"),
+                        rs.getBoolean("vaccinated")
+                );
+                pets.add(pet);
+            }
+
+            rs.close();
+            statement.close();
+
+        } catch (SQLException e) {
+            System.out.println("Search by age range failed!");
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.closeConnection(connection);
+        }
+
+        return pets;
+    }
+
+
+
+
+    public List<Pet> searchByMinAge(int minAge) {
+
+        List<Pet> pets = new ArrayList<>();
+
+        String sql =
+                "SELECT * FROM pet " +
+                        "WHERE age >= ? AND deleted = FALSE " +
+                        "ORDER BY age DESC";
+
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection == null) return pets;
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, minAge);
+
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                pets.add(new Pet(
+                        rs.getInt("pet_id"),
+                        rs.getString("name"),
+                        rs.getString("species"),
+                        rs.getInt("age"),
+                        rs.getBoolean("vaccinated")
+                ));
+            }
+
+            rs.close();
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.closeConnection(connection);
+        }
+
+        return pets;
+    }
+
+
+
+
+
 
 }
--- src/com/nurdaulet/vetclinic/menu/VetClinicMenu.java
+++ src/com/nurdaulet/vetclinic/menu/VetClinicMenu.java
@@ -1,23 +1,17 @@
 package com.nurdaulet.vetclinic.menu;
 
 import com.nurdaulet.vetclinic.*;
-import java.util.ArrayList;
+import com.nurdaulet.vetclinic.database.PetDAO;
 import java.util.Scanner;
 
 public class VetClinicMenu implements Menu {
 
-    private ArrayList<Person> people;
-    private ArrayList<Pet> pets;
     private Scanner scanner;
+    private PetDAO petDAO;
 
     public VetClinicMenu() {
-        people = new ArrayList<>();
-        pets = new ArrayList<>();
         scanner = new Scanner(System.in);
-
-        // test data
-        pets.add(new Pet(1, "Buddy", "Dog", 3, false));
-        people.add(new Veterinarian(1, "Dr. Aibek", 35, 201, "Dog", 6, true));
+        petDAO = new PetDAO();
     }
 
     @Override
@@ -25,8 +19,11 @@ public class VetClinicMenu implements Menu {
         System.out.println("=== VET CLINIC MENU ===");
         System.out.println("1. Add Pet");
         System.out.println("2. View All Pets");
-        System.out.println("3. Add Owner");
-        System.out.println("4. Add Veterinarian");
+        System.out.println("5. Update Pet");
+        System.out.println("6. Delete Pet");
+        System.out.println("7. Search Pet by Name");
+        System.out.println("8. Search Pet by Age Range");
+        System.out.println("9. Search Pet by Min Age");
         System.out.println("0. Exit");
     }
 
@@ -49,11 +46,20 @@ public class VetClinicMenu implements Menu {
                     case 2:
                         viewAllPets();
                         break;
-                    case 3:
-                        addOwner();
+                    case 5:
+                        updatePet();
                         break;
-                    case 4:
-                        addVeterinarian();
+                    case 6:
+                        deletePet();
+                        break;
+                    case 7:
+                        searchPetByName();
+                        break;
+                    case 8:
+                        searchPetByAgeRange();
+                        break;
+                    case 9:
+                        searchPetByMinAge();
                         break;
                     case 0:
                         running = false;
@@ -85,68 +91,127 @@ public class VetClinicMenu implements Menu {
         int age = scanner.nextInt();
         scanner.nextLine();
 
-        pets.add(new Pet(id, name, species, age, false));
+        Pet pet = new Pet(id, name, species, age, false);
+        petDAO.insertPet(pet);
         System.out.println("Pet added!");
     }
 
-    private void addOwner() {
-        System.out.print("Person ID: ");
-        int id = scanner.nextInt();
-        scanner.nextLine();
 
-        System.out.print("Name: ");
+    private void searchPetByName() {
+        System.out.print("Enter name: ");
         String name = scanner.nextLine();
 
-        System.out.print("Age: ");
-        int age = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("Phone: ");
-        String phone = scanner.nextLine();
-
-        System.out.print("Number of pets: ");
-        int petsCount = scanner.nextInt();
-        scanner.nextLine();
-
-        people.add(new Owner(id, name, age, phone, petsCount, petsCount >= 3));
-        System.out.println("Owner added!");
+        petDAO.searchPetsByName(name)
+                .forEach(System.out::println);
     }
 
-    private void addVeterinarian() {
-        System.out.print("Person ID: ");
-        int id = scanner.nextInt();
+    private void searchPetByAgeRange() {
+        System.out.print("Min age: ");
+        int min = scanner.nextInt();
+        System.out.print("Max age: ");
+        int max = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.print("Name: ");
-        String name = scanner.nextLine();
+        petDAO.searchPetsByAgeRange(min, max)
+                .forEach(System.out::println);
+    }
 
-        System.out.print("Age: ");
-        int age = scanner.nextInt();
+    private void searchPetByMinAge() {
+        System.out.print("Min age: ");
+        int min = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.print("Vet ID: ");
-        int vetId = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("Specialization: ");
-        String spec = scanner.nextLine();
-
-        System.out.print("Experience years: ");
-        int exp = scanner.nextInt();
-        scanner.nextLine();
-
-        people.add(new Veterinarian(id, name, age, vetId, spec, exp, true));
-        System.out.println("Veterinarian added!");
+        petDAO.searchByMinAge(min)
+                .forEach(System.out::println);
     }
 
     private void viewAllPets() {
-        if (pets.isEmpty()) {
-            System.out.println("No pets found.");
+        petDAO.getAllPets();
+    }
+
+
+    private boolean isAdmin = true;
+
+    private void updatePet() {
+
+        PetDAO dao = new PetDAO();
+
+        System.out.print("Enter Pet ID to update: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        Pet existingPet = dao.getPetById(id);
+
+        if (existingPet == null) {
+            System.out.println("No pet found with ID: " + id);
             return;
         }
 
-        for (Pet pet : pets) {
-            System.out.println(pet);
+        System.out.println("Current info:");
+        System.out.println(existingPet);
+
+        System.out.print("New name (" + existingPet.getName() + "): ");
+        String name = scanner.nextLine();
+        if (name.isBlank()) name = existingPet.getName();
+
+        System.out.print("New species (" + existingPet.getSpecies() + "): ");
+        String species = scanner.nextLine();
+        if (species.isBlank()) species = existingPet.getSpecies();
+
+        System.out.print("New age (" + existingPet.getAge() + "): ");
+        String ageInput = scanner.nextLine();
+        int age = ageInput.isBlank()
+                ? existingPet.getAge()
+                : Integer.parseInt(ageInput);
+
+        Pet updatedPet = new Pet(
+                id,
+                name,
+                species,
+                age,
+                existingPet.isVaccinated()
+        );
+
+        dao.updatePet(updatedPet);
+    }
+
+    private void deletePet() {
+
+        PetDAO dao = new PetDAO();
+
+        if (!isAdmin) {
+            System.out.println("Permission denied.");
+            return;
+        }
+
+        System.out.print("Enter Pet ID to delete: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        // 1. Load pet from database
+        Pet pet = dao.getPetById(id);
+
+        if (pet == null) {
+            System.out.println("No pet found with ID: " + id);
+            return;
+        }
+
+        // 2. Show what will be deleted
+        System.out.println("Pet to be deleted:");
+        System.out.println(pet);
+
+        // 3. Ask for confirmation
+        System.out.print("Are you sure? (yes/no): ");
+        String confirm = scanner.nextLine();
+
+        // 4. Delete only if confirmed
+        if (confirm.equalsIgnoreCase("yes")) {
+            dao.deletePet(id);
+        } else {
+            System.out.println("Deletion cancelled.");
         }
     }
+
+
+
 }
